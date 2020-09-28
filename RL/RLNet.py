e93b0a1b0b4209fbb00a6cc4fd8802858700b5ec
weissle <2016301500298@whu.edu.cn>
2020-09-28 09:26:08 +0800

    not finish

--- RL/RLNet.py
+++ RL/RLNet.py
@@ -1,29 +1,44 @@
 import torch
-from torch import nn,optim
+from torch import nn, optim
+
 
 class RLNet(nn.Module):
     def __init__(self):
         super(RLNet, self).__init__()
         self.conv = nn.Sequential(
             # 1 * 15 * 15
-            nn.Conv2d(1,32,9) #  32 * 7 * 7
+            nn.Conv2d(1, 32, 9)  # 32 * 7 * 7
             nn.ReLU(),
-            nn.Conv2d(32,256,5,padding=1) # 256 * 5 * 5
+            nn.Conv2d(32, 256, 5, padding=1)  # 256 * 5 * 5
             nn.ReLU(),
-            nn.MaxPool2d(3), # 256 * 3 * 3
-            nn.Conv2d(256,1024,3,1), # 1024 * 3 * 3
+            nn.MaxPool2d(3),  # 256 * 3 * 3
+            nn.Conv2d(256, 1024, 3, 1),  # 1024 * 3 * 3
             nn.ReLU(),
-            nn.Conv2d(1024,2048,3,1), # 2048 * 1 * 1
+            nn.Conv2d(1024, 2048, 3, 1),  # 2048 * 1 * 1
             nn.ReLU()
         )
         self.fc = nn.Sequential(
-            nn.Linear(2048,1024),
+            nn.Linear(2048, 1024),
             nn.ReLU(),
-            nn.Linear(1024,512)
+            nn.Linear(1024, 512)
             nn.ReLU(),
-            nn.Linear(512,15**2)
+            nn.Linear(512, 15**2)
         )
+
     def forward(self, board):
         first_ = self.conv(board)
         output = self.fc(first_)
         return output
+
+    @torch.no_grad()
+    def test(self, board):
+        player = board.who_should_place()
+        input = torch.from_numpy(board.board_)
+        probs = self.forward(input)
+        probs_softmax = nn.softmax(probs)
+        probs_rank_index = probs.argsort(probs_softmax.numpy())
+        for index in probs_rank_index[::-1]:
+            x = index / 15
+            y = index % 15
+            if(board.place(x,y)):
+                break
--- rule/GobangBase.py
+++ rule/GobangBase.py
@@ -9,6 +9,7 @@ class Gobang:
         self.board_ = np.zeros((x_len, y_len), dtype=int)
         self.over_ = False
         self.winner_ = None
+
     def gameover(self):
         return self.over_
 
@@ -17,40 +18,41 @@ class Gobang:
 
     def towin(self):
         self.over_ = True
-        if len(self.sequence_) %2 == 0:
+        if len(self.sequence_) % 2 == 0:
             self.winner_ = "White"
         else:
             self.winner_ = "Black"
-    def check(self,x, y, chess):
-        up_x,down_x = x,x
-        left_y,right_y =y,y
-        lu_lx,lu_ly = x,y
-        lu_rx,lu_ry = x,y
-        ru_lx,ru_ly = x,y
-        ru_rx,ru_ry = x,y
+
+    def check(self, x, y, chess):
+        up_x, down_x = x, x
+        left_y, right_y = y, y
+        lu_lx, lu_ly = x, y
+        lu_rx, lu_ry = x, y
+        ru_lx, ru_ly = x, y
+        ru_rx, ru_ry = x, y
 
         for i in range(5):
-            if up_x >= 0 and self.board_[up_x,y]==chess:
+            if up_x >= 0 and self.board_[up_x, y] == chess:
                 up_x -= 1
-            if down_x < self.x_len and self.board_[down_x,y]==chess:
-                down_x +=1
-            if left_y >= 0 and self.board_[x,left_y]==chess:
+            if down_x < self.x_len and self.board_[down_x, y] == chess:
+                down_x += 1
+            if left_y >= 0 and self.board_[x, left_y] == chess:
                 left_y -= 1
-            if right_y < self.y_len and self.board_[x,right_y]==chess:
-                right_y +=1
-            if lu_lx >= 0 and lu_ly >=0 and self.board_[lu_lx,lu_ly]==chess:
+            if right_y < self.y_len and self.board_[x, right_y] == chess:
+                right_y += 1
+            if lu_lx >= 0 and lu_ly >= 0 and self.board_[lu_lx, lu_ly] == chess:
                 lu_lx -= 1
                 lu_ly -= 1
-            if lu_rx < self.x_len and lu_ry < self.y_len and self.board_[lu_rx,lu_ry]==chess:
+            if lu_rx < self.x_len and lu_ry < self.y_len and self.board_[lu_rx, lu_ry] == chess:
                 lu_rx += 1
                 lu_ry += 1
-            if ru_lx < self.x_len and ru_ly >=0 and self.board_[ru_lx,ru_ly]==chess:
+            if ru_lx < self.x_len and ru_ly >= 0 and self.board_[ru_lx, ru_ly] == chess:
                 ru_lx += 1
                 ru_ly -= 1
-            if ru_rx >=0 and ru_ry < self.y_len and self.board_[ru_rx,ru_ry]==chess:
+            if ru_rx >= 0 and ru_ry < self.y_len and self.board_[ru_rx, ru_ry] == chess:
                 ru_rx -= 1
                 ru_ry += 1
-        if down_x - up_x >= 6 or right_y - left_y >= 6 or lu_rx - lu_lx >=6 or ru_lx - ru_rx >=6:
+        if down_x - up_x >= 6 or right_y - left_y >= 6 or lu_rx - lu_lx >= 6 or ru_lx - ru_rx >= 6:
             self.towin()
         '''        
         while up_x >= 0 and self.board_[up_x,y] == chess:
@@ -70,26 +72,46 @@ class Gobang:
             return
         '''
 
+    def placeable(self, x, y):
+        if self.over_ or x < 0 or y < 0 or x >= self.x_len or y >= self.y_len or self.board_[x, y] != 0:
+            return False
+        else:
+            return True
+
     def place(self, x, y):
-        if self.over_ or  x < 0 or y < 0 or x >= self.x_len or y >= self.y_len or self.board_[x, y] != 0 :
-            return
-        chess = None
-        if len(self.sequence_) % 2 == 0:
+        if placeable(x, y) == False:
+            return False
+        chess = self.who_should_place()
+        if chess == "Black":
             chess = 1
         else:
             chess = -1
-        self.sequence_.append((x,y))
+        self.sequence_.append((x, y))
         self.board_[x, y] = chess
-        self.check(x,y,chess)
-
+        self.check(x, y, chess)
+        return True
+'''
     def place_indexfrom1(self, x, y):
-        self.place(x-1,y-1)
-    
+        self.place(x-1, y-1)
+'''
     def who_should_place(self):
-        if len(self.sequence_) % 2 ==0:
+        if len(self.sequence_) % 2 == 0:
             return "Black"
         else:
             return "White"
-    
+
     def print_board(self):
         print(self.board_)
+
+class GobangAI(Gobang):
+    def __init__(self,x,y):
+        super().__init__(x,y)
+        self.board_white_ = self.board_ = np.zeros((x_len, y_len), dtype=int)
+    
+    def place(self,x,y):
+        if(super().place(x,y)):
+            point = self.sequence_[-1]
+            self.board_white_[point[0],point[1]] = -1 * self.board_[point[0],point[1]]
+            return True
+        else:
+            return False
